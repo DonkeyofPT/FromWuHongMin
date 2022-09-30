72c45cca9a27cc5add7a03f7861fb033a31897db
lbccjyx <1033070036@qq.com>
2022-09-30 13:54:46 +0800

    test

--- map/main.py
+++ map/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Wonz
 # 功能：主程序
+import time
 
 import pygame
 import sys
@@ -26,6 +27,102 @@ def print_text(font, x, y, text, color, shadow=True):
     imgText = font.render(text, True, color)
     screen.blit(imgText, (x,y))
 
+#重画迷宫
+def PrintAll(r_list):
+    # 画出去起点和终点的其他点
+    begin_point = [0, 0]
+    for i in range(maze.room_m):
+        for j in range(maze.room_n):
+            begin_point[0] = 25 + i * maze.room_size
+            begin_point[1] = 25 + j * maze.room_size
+            r_color = color.Black
+            maze.draw_room(screen, begin_point, r_list[i][j].walls, maze.room_size, r_color)
+    # 画起点
+    maze.draw_room(screen, [25, 25], [0, 0, 0, 1], maze.room_size, color.White)
+    # 画终点
+    maze.draw_room(screen, [25 + (maze.room_m - 1) * maze.room_size, 25 + (maze.room_n - 1) * maze.room_size],
+                        [0, 1, 0, 0], maze.room_size, color.White)
+    pygame.draw.circle(screen, color.Red, [25 + (maze.room_m ) * maze.room_size, 25 + (maze.room_n- 0.5) * maze.room_size], maze.room_size/3, 0)
+
+
+def PrintTotal(list_migong, list_user: list, strA: str):
+    font3 = pygame.font.Font(None, 99)
+    screen.fill(color.White)
+    PrintAll(list_migong)
+    for user in list_user:
+        screen.blit(user.GetUser(), user.GetPosTuple())
+
+    if len(strA) > 0:
+        print_text(font3, (25 + (maze.room_m - 1) * maze.room_size) / 2,
+                   (25 + (maze.room_n - 1) * maze.room_size) / 2, strA, color.Red, False)
+    pygame.display.flip()
+
+
+def PlayGame():
+    succflag = False
+    # 字体
+    font1 = pygame.font.Font(None, 25)
+    font2 = pygame.font.Font(None, 25)
+
+    # 清屏
+    screen.fill(color.White)
+
+    # 创建随机迷宫
+    r_list = maze.creat_map(maze.room_m, maze.room_n)
+    begin_room = r_list[0][0]
+    maze.creat_migong(r_list, begin_room)
+
+    # 画迷宫
+    PrintAll(r_list)
+
+    # 加载角色照片，并画角色
+    FirstUser = mapp.User("user.png")
+    list_users =[]
+    list_users.append(FirstUser)
+    screen.blit(FirstUser.GetUser(), FirstUser.GetPosTuple())
+
+    # 键盘控制角色移动
+    while True:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # 走到终点
+            elif (FirstUser.GetMathX() == maze.room_m - 1 and FirstUser.GetMathY() == maze.room_n - 1):
+                PrintTotal(r_list, list_users, "win")
+                succflag = True
+                for i in range(3):
+                    time.sleep(1)
+                    PrintTotal(r_list, list_users, f"{-i+3}")
+            # 键盘响应，只取按“→”键作为例子，“↑”、“↓”、“←”类似，只要改改参数即可
+            elif event.type == pygame.KEYDOWN:
+
+                # 瞎输continue 否则下面溢出
+                if (((-event.key + 1073741906) > 3) or ((-event.key + 1073741906) < 0)):
+                    continue
+
+                # 重新刷屏
+                PrintTotal(r_list, [], "")
+
+                print_text(font1, 25, 0, f"Steps:{FirstUser.GetSteps()}", color.Black, False)
+
+                wallPos = maze.ChangeList[-event.key + 1073741906]
+
+                # 无墙
+                if (r_list[FirstUser.GetMathX()][FirstUser.GetMathY()].walls[wallPos] == False):
+                    screen.blit(FirstUser.GetUser(), FirstUser.move(event.key))
+
+                # 有墙
+                elif (r_list[FirstUser.GetMathX()][FirstUser.GetMathY()].walls[wallPos] == True):
+                    FirstUser.AddStep()
+                    screen.blit(FirstUser.GetUser(), FirstUser.GetPosTuple())
+                    print_text(font2, 300, 0, "This is a wall!", color.Red, False)
+
+        pygame.display.flip()
+        if succflag:
+
+            succflag = False
+            PlayGame()
 
 # 游戏开始
 if __name__ == '__main__':
@@ -33,134 +130,5 @@ if __name__ == '__main__':
 
     screen = pygame.display.set_mode([screen_width, screen_height])
     pygame.display.set_caption('迷宫') # 游戏标题
-    global font1, font2, font3, font4 # 文字
-
-
-    clock = pygame.time.Clock()
-    fps = 20
-    screen.fill(color.White)
-
-
-    r_list = maze.room.creat_map(maze.room_m, maze.room_n)
-    begin_point = [0, 0]
-    begin_room = r_list[0][0]
-    maze.room.creat_migong(r_list, begin_room)
-    # 画出去起点和终点的其他点
-    for i in range(maze.room_m):
-        for j in range(maze.room_n):
-            begin_point[0] = 25 + i * maze.room_size
-            begin_point[1] = 25 + j * maze.room_size
-            r_color = color.Black
-            maze.room.draw_room(screen, begin_point, r_list[i][j].walls, maze.room_size, r_color)
-    # 画起点
-    maze.room.draw_room(screen, [25, 25], [0, 0, 0, 1], maze.room_size, color.White)
-    # 画终点
-    maze.room.draw_room(screen, [25 + (maze.room_m - 1) * maze.room_size, 25 + (maze.room_n - 1) * maze.room_size],
-                        [0, 1, 0, 0], maze.room_size, color.White)
-    pygame.draw.circle(screen, color.Red, [25 + (maze.room_m ) * maze.room_size, 25 + (maze.room_n- 0.5) * maze.room_size], maze.room_size/3, 0)
-
-    # 加载角色照片
-    FirstUser = mapp.User("user.png")
-    screen.blit(FirstUser.GetUser(), FirstUser.GetPosTuple())
-
-    # 键盘控制角色移动
-    font1 = pygame.font.Font(None, 25)
-    font2 = pygame.font.Font(None, 25)
-
-    while True:
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                sys.exit()
-            # 走到终点
-            elif(FirstUser.GetMathX() == maze.room_m-1 and FirstUser.GetMathY() == maze.room_n-1):
-                font4 = pygame.font.Font(None, 32)
-                print_text(font4, 350, 350, "Win" , color.Red, False)
-                break
-            # 键盘响应，只取按“→”键作为例子，“↑”、“↓”、“←”类似，只要改改参数即可
-
-            elif event.type == pygame.KEYDOWN:
-                screen.fill(color.White, (25, 0, 200, 25))  # x:25 y:0 width:200 height:25
-                screen.fill(color.White, (300, 0, 200, 25))  # x:300 y:0 width:200 height:25
-
-                #瞎输continue 否则下面溢出
-                if ( ((-event.key + 1073741906) > 3) or ((-event.key + 1073741906) < 0) ):
-                    continue
-
-                wallPos = maze.ChangeList[-event.key + 1073741906]
-                # 无墙
-                if(r_list[FirstUser.GetMathX()][FirstUser.GetMathY()].walls[wallPos] == False):
-                    screen.blit(FirstUser.GetUser(), FirstUser.move(event.key))
-                    textImage = font1.render(f"Steps:{FirstUser.GetSteps()}", True, "black", None)
-                    screen.blit(textImage, (25, 0))
-                # 有墙
-                elif (r_list[FirstUser.GetMathX()][FirstUser.GetMathY()].walls[wallPos] == True):
-                    FirstUser.AddStep()
-                    textImage = font1.render(f"Steps:{FirstUser.GetSteps()}", True, "black", None)
-                    textImage2 = font2.render("This is a wall!", True, "black", None)
-                    screen.blit(textImage, (25, 0))
-                    screen.blit(textImage2, (300, 0))
-                    screen.blit(FirstUser.GetUser(), FirstUser.GetPosTuple())
-        pygame.display.flip()
-"""
-            # 鼠标响应，执行 AI 程序
-            elif event.type == pygame.MOUSEBUTTONUP:
-                font3 = pygame.font.Font(None, 32)
-                print_text(font3, 750, 0, "AI", color.Red)
-                pygame.display.flip()
-                start_x = 0
-                start_y = 0
-                steps = 0
-                for i in range(1000000000):
-                    if (start_x == maze.room_m-1 and start_y == maze.room_n-1):
-                        font4 = pygame.font.Font(None, 32)
-                        print_text(font4, 350, 350, "Win", color.Red)
-                        break
-                    d = random.randint(1,4)
-                    # 上边无墙
-                    if (d == 1 and 0 <= start_x <= maze.room_m - 1 and 0 <= start_y <= maze.room_n - 1 and
-                            r_list[start_x][start_y].walls[0] == False): # 在迷宫地图范围内且上边无墙
-                        start_y -= 1
-                        steps += 1
-                        font1 = pygame.font.Font(None, 32)
-                        screen.fill(color.White, (25, 0, 200, 25))  # x:25 y:0 width:200 height:25
-                        screen.fill(color.White, (300, 0, 200, 25))  # x:300 y:0 width:200 height:25
-                        print_text(font1, 25, 0, "Steps:" + str(steps), color.Black) # 步数统计
-                        pygame.display.flip()
-                        screen.blit(user, ((start_x+2) * maze.room_size, (start_y+2) * maze.room_size))
-                    # 右边无墙
-                    if (d == 2 and 0 <= start_x <= maze.room_m - 1 and 0 <= start_y <= maze.room_n - 1 and
-                            r_list[start_x][start_y].walls[1] == False):
-                        start_x += 1
-                        steps += 1
-                        font1 = pygame.font.Font(None, 32)
-                        screen.fill(color.White, (25, 0, 200, 25))
-                        screen.fill(color.White, (300, 0, 200, 25))
-                        print_text(font1, 25, 0, "Steps:" + str(steps), color.Black)
-                        pygame.display.flip()
-                        screen.blit(user, ((start_x+2) * maze.room_size, (start_y+2) * maze.room_size))
-                    # 下边无墙
-                    if (d == 3 and 0 <= start_x <= maze.room_m - 1 and 0 <= start_y <= maze.room_n - 1 and
-                            r_list[start_x][start_y].walls[2] == False):
-                        start_y += 1
-                        steps += 1
-                        font1 = pygame.font.Font(None, 32)
-                        screen.fill(color.White, (25, 0, 200, 25))
-                        screen.fill(color.White, (300, 0, 200, 25))
-                        print_text(font1, 25, 0, "Steps:" + str(steps), color.Black)
-                        pygame.display.flip()
-                        screen.blit(user, ((start_x+2) * maze.room_size, (start_y+2) * maze.room_size))
-                    # 左边无墙
-                    if (d == 4 and 0 <= start_x <= maze.room_m - 1 and 0 <= start_y <= maze.room_n - 1 and
-                            r_list[start_x][start_y].walls[3] == False):
-                        start_x -= 1
-                        steps += 1
-                        font1 = pygame.font.Font(None, 32)
-                        screen.fill(color.White, (25, 0, 200, 25))
-                        screen.fill(color.White, (300, 0, 200, 25))
-                        print_text(font1, 25, 0, "Steps:" + str(steps), color.Black)
-                        pygame.display.flip()
-                        screen.blit(user, ((start_x+2) * maze.room_size, (start_y+2) * maze.room_size))
-"""
-    #更新屏幕
+    global font1, font2, font3 # 文字
+    PlayGame()
--- map/maze.py
+++ map/maze.py
@@ -13,9 +13,9 @@ import color
 
 # 设置迷宫的大小
 global room_m, room_n, room_size
-room_m = 30
-room_n = 30
-room_size = 20 # 每个小房间的大小
+room_m = 10
+room_n = 10
+room_size = 30 # 每个小房间的大小
 
 
 # 设置屏幕宽度和高度为全局变量
@@ -37,89 +37,119 @@ class room():
         self.x = x
         self.y = y
 
-    # 画迷宫
-    def draw_room(screen, begin_point, walls, size, r_color):
-        n = 0
-        # 一个小房间的四面墙
-        for wall in walls:
-            x = begin_point[0] # 迷宫起点的 x 坐标
-            y = begin_point[1] # 迷宫起点的 y 坐标
-            n += 1
-            if n == 1 and wall: # x → x + size 是墙
-                pygame.draw.line(screen, r_color, (x, y), (x + size, y))
-            if n == 2 and wall:
-                pygame.draw.line(screen, r_color, (x + size, y), (x + size, y + size))
-            if n == 3 and wall:
-                pygame.draw.line(screen, r_color, (x + size, y + size), (x, y + size))
-            if n == 4 and wall:
-                pygame.draw.line(screen, r_color, (x, y + size), (x, y))
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
+# 画迷宫
+def draw_room(screen, begin_point, walls, size, r_color):
+    n = 0
+    # 一个小房间的四面墙
+    for wall in walls:
+        x = begin_point[0] # 迷宫起点的 x 坐标
+        y = begin_point[1] # 迷宫起点的 y 坐标
+        n += 1
+        if n == 1 and wall: # x → x + size 是墙
+            pygame.draw.line(screen, r_color, (x, y), (x + size, y))
+        if n == 2 and wall:
+            pygame.draw.line(screen, r_color, (x + size, y), (x + size, y + size))
+        if n == 3 and wall:
+            pygame.draw.line(screen, r_color, (x + size, y + size), (x, y + size))
+        if n == 4 and wall:
+            pygame.draw.line(screen, r_color, (x, y + size), (x, y))
 
 
-    # 生成迷宫地图
-    def creat_map(m, n):
-        room_list = [[0 for col in range(n)] for row in range(m)] # 二维数组: m*n
-        for i in range(m):
-            for j in range(n):
-                room_list[i][j] = room(i, j)
-        return room_list
+# 生成迷宫地图
+def creat_map(m, n):
+    room_list = [[0 for col in range(n)] for row in range(m)] # 二维数组: m*n
+    for i in range(m):
+        for j in range(n):
+            room_list[i][j] = room(i, j)
+    return room_list
 
 
-    # 获取下一个房间
-    def get_next_room(room_list, room):
-        temp_rooms = {1: None,
-                      2: None,
-                      3: None,
-                      4: None}
-        temp_room_count = 0
-        # 判断上下左右四个方向的小房间有没有被访问，没有的话就加入 temp_rooms[]
-        if (not room.y - 1 < 0) and (not room_list[room.x][room.y - 1].visited): # 上边没被访问
-            temp_rooms[1] = room_list[room.x][room.y - 1]
-            temp_room_count += 1
-        if (not room.x + 1 > room_m - 1) and (not room_list[room.x + 1][room.y].visited): # 右边
-            temp_rooms[2] = room_list[room.x + 1][room.y]
-            temp_room_count += 1
-        if (not room.y + 1 > room_n - 1) and (not room_list[room.x][room.y + 1].visited): # 下边
-            temp_rooms[3] = room_list[room.x][room.y + 1]
-            temp_room_count += 1
-        if (not room.x - 1 < 0) and (not room_list[room.x - 1][room.y].visited): # 左边
-            temp_rooms[4] = room_list[room.x - 1][room.y]
-            temp_room_count += 1
 
-        if temp_room_count > 0:
-            while True:
-                room_id = randint(1, 4) # 随机生成，指定某一边没有墙
-                if temp_rooms[room_id]:
-                    next_room = temp_rooms[room_id]
-                    if room_id == 1:
-                        room.walls[0] = 0  # # 上0 下2 左3 右1  当前房间的上边没有墙
-                        next_room.walls[2] = 0  # 上面房间的下边没有墙
-                    if room_id == 2:
-                        room.walls[1] = 0 #当前房间的右边
-                        next_room.walls[3] = 0 #上面房间的左边
-                        room.walls[2] = 0
-                        next_room.walls[0] = 0
-                    if room_id == 3:
-                        room.walls[2] = 0
-                        next_room.walls[0] = 0
-                    if room_id == 4:
-                        room.walls[3] = 0
-                        next_room.walls[1] = 0
-                    break
-            return next_room
-        else:
-            return None
+def check_map(room_list: list):
+    for i in range(room_m):
+        for j in range(room_n):
+            room = room_list[i][j]
+            # 上
+            if i == 0:
+                room.walls[3] = 1  # 左
+            if i == room_m-1:
+                room.walls[1] = 1  # 右
+            if j == 0:
+                room.walls[0] = 1  # 上
+            if j == room_n-1:
+                room.walls[2] = 1  # 下
+
+            if j < room_n-1:
+                if room_list[i][j].walls[2] == 1:
+                    room_list[i][j+1].walls[0] = 1
+                if room_list[i][j+1].walls[0] == 1:
+                    room_list[i][j].walls[2] = 1
 
 
-    # 创建迷宫
-    def creat_migong(room_list, next_room, temp_yes_rooms=[]):
+
+# 获取下一个房间
+def get_next_room(room_list, room):
+    temp_rooms = {1: None,
+                  2: None,
+                  3: None,
+                  4: None}
+    temp_room_count = 0
+    # 判断上下左右四个方向的小房间有没有被访问，没有的话就加入 temp_rooms[]
+    if (not room.y - 1 < 0) and (not room_list[room.x][room.y - 1].visited): # 上边没被访问
+        temp_rooms[1] = room_list[room.x][room.y - 1]
+        temp_room_count += 1
+    if (not room.x + 1 > room_m - 1) and (not room_list[room.x + 1][room.y].visited): # 右边
+        temp_rooms[2] = room_list[room.x + 1][room.y]
+        temp_room_count += 1
+    if (not room.y + 1 > room_n - 1) and (not room_list[room.x][room.y + 1].visited): # 下边
+        temp_rooms[3] = room_list[room.x][room.y + 1]
+        temp_room_count += 1
+    if (not room.x - 1 < 0) and (not room_list[room.x - 1][room.y].visited): # 左边
+        temp_rooms[4] = room_list[room.x - 1][room.y]
+        temp_room_count += 1
+
+    if temp_room_count > 0:
         while True:
-            if next_room:
-                # 下一房间未被访问
-                if not next_room.visited:
-                    next_room.visited = True
-                    temp_yes_rooms.append(next_room)
-                next_room = room.get_next_room(room_list, next_room)
-            else:
-                next_room = temp_yes_rooms.pop()  # 否则出栈
-                if len(temp_yes_rooms) == 0:
-                    break
+            room_id = randint(1, 4) # 随机生成，指定某一边没有墙
+            if temp_rooms[room_id]:
+                next_room = temp_rooms[room_id]
+                if room_id == 1:
+                    room.walls[0] = 0  # # 上0 下2 左3 右1  当前房间的上边没有墙
+                    next_room.walls[2] = 0  # 上面房间的下边没有墙
+                if room_id == 2:
+                    room.walls[1] = 0 #当前房间的右边
+                    next_room.walls[3] = 0 #上面房间的左边
+                    room.walls[2] = 0
+                    next_room.walls[0] = 0
+                if room_id == 3:
+                    room.walls[2] = 0
+                    next_room.walls[0] = 0
+                if room_id == 4:
+                    room.walls[3] = 0
+                    next_room.walls[1] = 0
+                break
+        return next_room
+    else:
+        return None
+
+
+# 创建迷宫
+def creat_migong(room_list, next_room, temp_yes_rooms=[]):
+    while True:
+        if next_room:
+            # 下一房间未被访问
+            if not next_room.visited:
+                next_room.visited = True
+                temp_yes_rooms.append(next_room)
+            next_room = get_next_room(room_list, next_room)
+        else:
+            next_room = temp_yes_rooms.pop()  # 否则出栈
+            if len(temp_yes_rooms) == 0:
+                break
+    check_map(room_list)
